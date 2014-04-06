bd54fc23305e841111d533466bfe188100c22898
Henry Spindell <henryspindell@gmail.com>
2014-04-06 15:52:29 -0500

    Results to table

--- js/app.js
+++ js/app.js
@@ -4,32 +4,9 @@
 *
 *
 */
+$(document).ready(function(){
 
-//get dropdown category for search 
-$("#search_cat").click(function(){
-	var category = $("#search_cat").val();
-});
-
-
-$("#search").click(function(){
-
-var query = new Parse.Query("tutor");
-query.equalTo("Subject", "Algorithms");
-query.find({
-  success: function(results) {
-    // results is an array of Parse.Object.
-    console.log("success");
-    console.log(results[0]['attributes']['Name']);
-  },
-
-  error: function(error) {
-    // error is an instance of Parse.Error.
-  }
-	});
-});
-
-$(function(){
-	var suggestion =[	
+var suggestion =[	
 	  { value: 'Game Design', data: 'GD' },
     { value: 'Data Structures', data: 'DS' },
     { value: 'C++', data: 'C++' },
@@ -37,37 +14,46 @@ $(function(){
     { value: 'Algorithms', data: 'AL' },
 	];
 
-$('#autocomplete').autocomplete({
+  $('#autocomplete').autocomplete({
     lookup: suggestion,
     onSelect: function (suggestion) {
       query_parse(suggestion);
-   //   var thehtml = '<strong>you have looked up</strong> ' + suggestion.value;
-   //   $('#outputcontent').html(thehtml);
     }
   });
   
-});
 
-function query_parse(suggestion){
-  var tutor = Parse.Object.extend("tutor");
-  var query = new Parse.Query(tutor);
-  query.equalTo("Subject", suggestion.value);
-  
-  query.find({
-  success: function(results) {
-    console.log("Successfully retrieved ");
-    console.log(results[0]['attributes']['Name']);
-    console.log(results[0]['attributes']['Subject']);
-    console.log(results[0]['attributes']['email']);
-
-     var result = 'Tutor ' +results[0]['attributes']['Name'] +' is available';
-     var result1= ' His email is: ' + results[0]['attributes']['email'];
-     $('#outputcontent').html(result+"<br>"+result1);
-  },
-  error: function(error) {
-    alert("Error: ");
-  }
-});
-
+function display_tutor(tutor)
+{
+   var name = tutor['attributes']['Name'];
+   var subject = tutor['attributes']['Subject'];
+   var price = '$0.00';
+   var email = tutor['attributes']['email'];
+   $('#results > tbody:last').append('<tr><td>'+name+'</td><td>'+subject+'</td><td>'+price+'</td><td>'+email+'</td></tr>');
 }
 
+
+function query_parse(suggestion)
+{
+  var tutor = Parse.Object.extend("tutor");
+  var query = new Parse.Query(tutor);
+//query.equalTo("Subject", suggestion.value);
+  
+  query.find({
+    success: function(results) {
+      console.log("Successfully retrieved \n"+ JSON.stringify(results,null,2));
+
+      for(var i=0;i<results.length;i++) display_tutor(results[i]);
+
+    },
+    error: function(error){
+      alert("Error: ");
+    }
+  });
+}
+
+
+
+
+
+
+});
