40a17faf53a4a0a07bad730ec92b4dd5f577c4b7
arundhatijaswal <arundhatijaswal2013@u.northwestern.edu>
2014-04-10 23:22:21 -0500

    tweaked display to remove horizontal scroll

--- js/app.js
+++ js/app.js
@@ -41,6 +41,7 @@ $(document).ready(function(){
     next.html(firstname+"<br>"+subject+"<br>"+price+"<br>");
 
     $('#datadiv').append("<div class='row'><div class='col-sm-4'><div class='media'><div class='row '><div class='media-img  pull-left  col-sm-5  col-md-4 '><img src='./icons/artwork-source.png' alt='About'  width='50' height='50' /></div><div class='media-body   col-sm-7 col-md-8  '><h4 class='media-heading'>"+firstname+"</h4><p class='hidden-sm'>Subject: "+subject+"</p><p class='hidden-sm'>Hourly rate: $"+price+"</p></div></div></div></div></div><hr>");
+    $('#datadiv').append("<div class='row'><div class='col-sm-2'><div class='row '><div class='media-img  pull-left  col-sm-5  col-md-4 '><img src='./icons/artwork-source.png' alt='About'  width='50' height='50' /></div><div class='media-body   col-sm-7 col-md-8  '><h4 class='media-heading'>"+name+"</h4><p class='hidden-sm'>Subject: "+subject+"</p><p class='hidden-sm'>Hourly rate: $"+price+"</p></div></div></div></div><hr>");
     // $('#datadiv').append("<div>"+firstname+"<br>"+subject+"<br>"+price+"<hr></div>");
 
     $('#results > tbody:last').append("<tr ><td>"+firstname+"</td><td>"+subject+"</td><td>"+price+"</td></tr>");
@@ -59,6 +60,11 @@ $(document).ready(function(){
 
         $('#results > tbody:last').html('');
         for(var i=0;i<results.length;i++) display_tutor(results[i]);
+        $('#datadiv').html('<hr>');
+        for(var i=0;i<results.length;i++) 
+        {
+          display_tutor(results[i]);
+        }
       },
       error: function(error){
         alert("Error: ");
