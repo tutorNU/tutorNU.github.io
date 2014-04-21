d560fdb0495f1dab671e1d4a4b4d183e25ba2cff
brian-0114 <limit56@gmail.com>
2014-04-21 17:39:18 -0500

    added auto form filler for demo day

--- js/create_profile.js
+++ js/create_profile.js
@@ -39,4 +39,16 @@ $(document).ready(function(){
  	 		}
 		})
 	})
+   $("#filler").click(function(){
+   	document.getElementById("NAME").value = "John Smith";
+   	document.getElementById("rate").value =15;
+	document.getElementById("EMAIL").value = "JohnSmith2000@gmail.com";
+	document.getElementById("SUBJECT").value = "Software Management"
+	document.getElementById("MAJOR").value = "Computer Science"
+	document.getElementById("YEAR").value = "2010"
+	document.getElementById("AVAIL").value = "free on Monday from 2:00pm to 5:00pm, Tuesday from 12:00pm to 2:00pm, Friday 10:00am to 12:00pm";
+	document.getElementById("EXP").value ="highly experience in software management";
+	document.getElementById("Ques").value = "do you have any HTML5, CSS, Javascript skills and experience in working in teams?";
+   })
+	
 });
