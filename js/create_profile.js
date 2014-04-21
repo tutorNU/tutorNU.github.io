8742ea3513444f8d961feb928e8609e78bd6ab20
arundhatijaswal <arundhatijaswal2013@u.northwestern.edu>
2014-04-21 18:09:04 -0500

    tweaked default values for demo

--- js/create_profile.js
+++ js/create_profile.js
@@ -40,15 +40,15 @@ $(document).ready(function(){
 		})
 	})
    $("#filler").click(function(){
-   	document.getElementById("NAME").value = "John Smith";
+   	document.getElementById("NAME").value = "Justin Reay";
    	document.getElementById("rate").value =15;
-	document.getElementById("EMAIL").value = "JohnSmith2000@gmail.com";
-	document.getElementById("SUBJECT").value = "Software Management"
+	document.getElementById("EMAIL").value = "JustinReay2011@u.northwestern.edu";
+	document.getElementById("SUBJECT").value = "Game Design"
 	document.getElementById("MAJOR").value = "Computer Science"
-	document.getElementById("YEAR").value = "2010"
+	document.getElementById("YEAR").value = "Junior"
 	document.getElementById("AVAIL").value = "free on Monday from 2:00pm to 5:00pm, Tuesday from 12:00pm to 2:00pm, Friday 10:00am to 12:00pm";
-	document.getElementById("EXP").value ="highly experience in software management";
-	document.getElementById("Ques").value = "do you have any HTML5, CSS, Javascript skills and experience in working in teams?";
+	document.getElementById("EXP").value ="Highly experienced in iOS game development";
+	document.getElementById("Ques").value = "Do you have any mobile development experience?";
    })
 	
 });
