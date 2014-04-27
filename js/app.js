9bec550d9f4d761a89a3a1b9612bc5e6b5edb080
Henry Spindell <henryspindell@gmail.com>
2014-04-27 16:13:48 -0500

    make posting smaller

--- js/app.js
+++ js/app.js
@@ -25,12 +25,12 @@ $(document).ready(function(){
           "<div class='col-sm-2'>"+
             "<div class='row'>"+
               "<div class='media-img  pull-left  col-sm-5  col-md-3'>"+
-                "<img src='./icons/artwork-source.png' alt='About'  width='50' height='50' />"+
+                "<img src='./icons/artwork-source.png' alt='About'  width='45' height='45' />"+
               "</div>"+
               "<div class='media-body  col-sm-7 col-md-8  '>"+
                 "<h4 class='media-heading'>"+name+"</h4>"+
-                "<p class='hidden-sm'>"+subject+"</p>"+
-                "<p class='hidden-sm'>Hourly rate: $"+price+"</p>"+
+                "<p class='hidden-sm'>Major: "+major+"<span class='pricebox pull-right'>$"+price+"/hr</span>"+
+                "<br/>Subject: "+subject+"</p>"+
               "</div>"+
             "</div>"+
           "</div>"+
