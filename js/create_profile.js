0d1420896dc7e0f71e63d909676db28070779984
Henry Spindell <henryspindell@gmail.com>
2014-04-29 15:33:53 -0500

    cleanup

--- js/create_profile.js
+++ js/create_profile.js
@@ -67,27 +67,25 @@ $(document).ready(function(){
     			// The save failed.
     			// error is a Parse.Error with an error code and description.
  	 		}
-			})
-		}
+		})
 	})
+
    $("#filler").click(function(){
-   	document.getElementById("NAME").value = "Justin Reay";
-   	document.getElementById("rate").value =15;
-	document.getElementById("EMAIL").value = "JustinReay2011@u.northwestern.edu";
-	document.getElementById("SUBJECT").value = "Game Design"
-	document.getElementById("MAJOR").value = "Computer Science"
-	document.getElementById("YEAR").value = "Junior"
-	document.getElementById("AVAIL").value = "Free on Monday from 2:00pm to 5:00pm, Tuesday from 12:00pm to 2:00pm, Friday 10:00am to 12:00pm";
-	document.getElementById("EXP").value ="TA for Professor Horswill, did some math tutoring in high school.";
-	$('#Ques').val("What previous experience do you have? What is your ultimate goal for learning this skill?");
-	document.getElementById("class").value = "EECS 394";
+   	$("#NAME").val("Justin Reay");
+   	$("#rate").val(15);
+		$("#EMAIL").val("JustinReay2011@u.northwestern.edu");
+		$("#SUBJECT").val("Game Design");
+		$("#MAJOR").val("Computer Science");
+		$("#YEAR").val("Junior");
+		$("#AVAIL").val("Free on Monday from 2:00pm to 5:00pm, Tuesday from 12:00pm to 2:00pm, Friday 10:00am to 12:00pm");
+		$("#EXP").val("TA for Professor Horswill, did some math tutoring in high school.");
+		$('#Ques').val("What previous experience do you have? What is your ultimate goal for learning this skill?");
+		$("#class").val("EECS 394");
    })
 
    $.fn.log = function() {
-  console.log.apply(console, this);
-  return this;
-};
+  		console.log.apply(console, this);
+  		return this;
+	};
 
-   
-	
 });
