ba1867386053df260fcc47cedbdb81bb9b779685
brian-0114 <limit56@gmail.com>
2014-04-29 14:21:05 -0500

    logo

--- js/header.js
+++ js/header.js
@@ -16,7 +16,7 @@ document.write(
     "<link rel='apple-touch-icon' sizes='120x120' href='./icons/ios/apple-touch-icon-144x144-precomposed.png'>"+
     "<link rel='apple-touch-icon' sizes='152x152' href='./icons/ios/Icon-72@2x.png'>"+
 
-    "<link rel='apple-touch-startup-image' href='./icons/TutorNU_logo640x1096.png'/>"+
+    "<link rel='apple-touch-startup-image' href='./icons/TutorNU_Logo640x1096.png'/>"+
     "<link href='./css/bootstrap.min.css' rel='stylesheet'>"+
 
 
