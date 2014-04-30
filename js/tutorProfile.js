2ee8c5b1a5233f96306336b19325885e69c47712
brian-0114 <limit56@gmail.com>
2014-04-30 12:47:24 -0500

    edit profile

--- js/tutorProfile.js
+++ js/tutorProfile.js
@@ -5,66 +5,66 @@
 
 
 $(document).ready(function(){
-	$('#header .container').html('');
+  $('#header .container').html('');
 
-	/*$(document).on("click", ".back", function(){
-   	window.history.go(-1);
-  	});*/
+  /*$(document).on("click", ".back", function(){
+    window.history.go(-1);
+    });*/
        
 
-	var tutorDB = Parse.Object.extend("tutor");
+  var tutorDB = Parse.Object.extend("tutor");
   var query = new Parse.Query(tutorDB);
 
-	var url = location.href;
+  var url = location.href;
 
-	var searchname = url.substring(url.indexOf('#')+1,url.length);
+  var searchname = url.substring(url.indexOf('#')+1,url.length);
 
    query.equalTo("Name", searchname);
 
    query.find({
-   	success: function(results){
-   		//fill in default person for development
-   		var name = "John Smith"; 
-    	var major = "Finance";
-   		var subjects = ["Econometrics"];
-   		var email = "johnsmith@gmail.com";
-   		var classes = "EECS 110";
+    success: function(results){
+      //fill in default person for development
+      var name = "John Smith"; 
+      var major = "Finance";
+      var subjects = ["Econometrics"];
+      var email = "johnsmith@gmail.com";
+      var classes = "EECS 110";
       var year = "Senior";
-   		var rating= [{grade:4.3,review:"John is a decent tutor, but he goes too fast sometimes.", who:"Tim"}];
+      var rating= [{grade:4.3,review:"John is a decent tutor, but he goes too fast sometimes.", who:"Tim"}];
       var questions = "";
       var password ="johnsmith";
 
-   		if(results[0]){
-   			name = results[0]['attributes']['Name'];
-   			major = results[0]['attributes']['major'];
-   			subjects = [results[0]['attributes']['Subject']];
-   			email= results[0]['attributes']['email'];
-   			year = results[0]['attributes']['Year'];
-   			rating = results[0]['attributes']['Rating'];
+      if(results[0]){
+        name = results[0]['attributes']['Name'];
+        major = results[0]['attributes']['major'];
+        subjects = [results[0]['attributes']['Subject']];
+        email= results[0]['attributes']['email'];
+        year = results[0]['attributes']['Year'];
+        rating = results[0]['attributes']['Rating'];
         questions = results[0]['attributes']['Question']; 
         classes= results[0]['attributes']['Classes'];
         availability = results[0]['attributes']['Availability'];
         experience = results[0]['attributes']['Experience']; 
         password = results[0]['attributes']['password'];
         Email = email;
-   		}
+      }
       
-   		//<span class='glyphicon glyphicon-chevron-left'></span> <--back button
-			$('#header .container').html("<center><h3>"+name+"</h3></center>");
+      //<span class='glyphicon glyphicon-chevron-left'></span> <--back button
+      $('#header .container').html("<center><h3>"+name+"</h3></center>");
      
-			$('#aboutname').html(name.split(' ')[0]);
-			$('#major').html(major);
-			$('#year').html(year);
+      $('#aboutname').html(name.split(' ')[0]);
+      $('#major').html(major);
+      $('#year').html(year);
       if(!questions) questions = "No questions.";
-			$('#email').html("<a href='mailto:tutor@gmail.com?Subject=Tutoring&body=Dear Student,%0D%0A%0D%0AThank you for your interest in my tutoring services. Please answer the questions below to help me better understand what you are looking for."+
+      $('#email').html("<a href='mailto:tutor@gmail.com?Subject=Tutoring&body=Dear Student,%0D%0A%0D%0AThank you for your interest in my tutoring services. Please answer the questions below to help me better understand what you are looking for."+
         "%0D%0A%0D%0A"+  questions  +"%0D%0A%0D%0A-"+name.split(' ')[0]+"%0D%0A%0D%0A===========================================================' target='_blank'>Send "+
           name.split(' ')[0]+
           " an email!</span></a>");
 
-			if(subjects) for(var i=0;i<subjects.length;i++) $('#skills .panel-body ul').append("<li>"+subjects[i]+"</li>");
+      if(subjects) for(var i=0;i<subjects.length;i++) $('#skills .panel-body ul').append("<li>"+subjects[i]+"</li>");
       else $('#skills .panel-body').html("No subjects listed.");
 
-			if(rating) for(var i=0;i<rating.length;i++){$('#reviews .panel-body').append("<p><b>("+rating[i]['grade']+")</b>  "+rating[i]['review']+"<br/>  - "+rating[i]['who']+"<br/><br/>");}
+      if(rating) for(var i=0;i<rating.length;i++){$('#reviews .panel-body').append("<p><b>("+rating[i]['grade']+")</b>  "+rating[i]['review']+"<br/>  - "+rating[i]['who']+"<br/><br/>");}
       else $('#reviews .panel-body').html("<b>No reviews yet.</b>");
 
       if(classes) $('#class .panel-body').html(classes);
@@ -75,11 +75,11 @@ $(document).ready(function(){
 
       // if(experience) $('#experience .panel-body').html(experience);
       // else $('#experience .panel-body').html("<b>No experience provided.</b>");
-  		},
+      },
 
-		error: function(err){
-      	$('#header .container').html("<center><h3>Error</h3></center>");
-    	}
+    error: function(err){
+        $('#header .container').html("<center><h3>Error</h3></center>");
+      }
 
    });
 
@@ -98,6 +98,7 @@ $(document).ready(function(){
 
    query.find({
   success: function(results) {
+     
        if(results[0]['attributes']['email']== Email &&
         results[0]['attributes']['pwd'] == password){
         name_ = results[0]['attributes']['Name'];
@@ -111,25 +112,19 @@ $(document).ready(function(){
         availability_ = results[0]['attributes']['Availability'];
         experience_= results[0]['attributes']['Experience']; 
         password_ = results[0]['attributes']['pwd'];
+        id = results[0].id;
+       
 
         
-        location.href = 'https://tutorNU.github.io/edit_profile.html';
+        location.href = "./edit_profile.html#"+id;
+
+
+        
       
-        console.log(name_);
-        console.log(major_);
-        console.log(subjects_);
-        console.log(password_);
-        document.getElementById("_NAME_").value =name_;
-        $('#MAJOR').val()=major_;
-        $('#SUBJECT').val()=subjects_;
-        $('#EMAIL').val()=email_;
-        $('#YEAR').val()=year_;
-        $('#rate').val()=rating_;
-        $('#Ques').val()=questions_;
-        $('#class').val()=classes_;
-        $('#AVAIL').val()=availability_;
-        $('#EXP').val()=experience_;
-        $('#password').val()=password_;
+      
+       
+
+        
       }
      
     
